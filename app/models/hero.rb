2b0445ff5344163e3e4050bc1b1dc30a98b96d2b
Sophie Gu <sophieqgu@gmail.com>
2019-09-05 04:24:35 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,9 +1,15 @@
 class Hero 
   attr_accessor :name, :power, :biography 
+  @@all = []
   
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @biography = params [:biography]
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
   end 
 end 
