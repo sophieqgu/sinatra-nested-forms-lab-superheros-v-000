2b04b93a96184b43a73054fa1745fd000404b9f9
Sophie Gu <sophieqgu@gmail.com>
2019-09-05 04:35:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ require_relative '../models/hero'
 
 class App < Sinatra::Base
 
-    #set :views, Proc.new { File.join(root, "../views/") }
+    set :views, Proc.new { File.join(root, "../views/") }
     
     get '/' do 
       erb :'super_hero'
